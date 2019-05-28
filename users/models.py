cc1159d3b2089fe520d8c941be02f3578d5445fc
krysnuvadga <tetechris20@gmail.com>
2019-05-28 00:54:11 -0400

    update profile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -7,7 +8,17 @@ class Profile(models.Model):
     """docstring for Profile."""
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField()
     image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
